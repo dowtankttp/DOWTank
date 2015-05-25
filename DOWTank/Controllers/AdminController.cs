66d9ca85845b36ed99a696cad1d6e77f81192e24
aamir-poswal <aamir.pucit@gmail.com>
2015-05-26 00:38:20 +0500

    contacts table with ajax object data source

---
+++ DOWTank/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DOWTank.Common;
+using DOWTank.Core.Domain.TANK_usp_sel;
+using DOWTank.Core.Service;
+
+namespace DOWTank.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly IUtilityService _utilityService;
+        private readonly ISharedFunctions _sharedFunctions;
+
+        public AdminController(IUtilityService utilityService, ISharedFunctions sharedFunctions)
+        {
+            _utilityService = utilityService;
+            _sharedFunctions = sharedFunctions;
+        }
+
+        #region Contacts
+
+        [HttpGet]
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetContacts()
+        {
+            // database call
+
+            var TANK_usp_sel_ContactUPD_spParams = new TANK_usp_sel_ContactUPD_spParams()
+            {
+                //TODO: re-factor it later from hard coded
+                InstallID = 1,
+                MajorLocationID = 1
+            };
+            DataTable dataTable = _utilityService.ExecStoredProcedureForDataTable("TANK_usp_sel_ContactUPD", TANK_usp_sel_ContactUPD_spParams);
+
+            var data = (from p in dataTable.AsEnumerable()
+                        select new
+                        {
+                            Id = p.Field<int>("Key"),
+                            LastName = p.Field<string>("Last Name*"),
+                            FirstName = p.Field<string>("First Name*"),
+                            Phone = p.Field<string>("Phone"),
+                            Ext = p.Field<string>("Ext"),
+                        }).ToList();
+
+            //# database call
+            //return Json(new { data = new[] { data } }, JsonRequestBehavior.AllowGet);
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion Contacts
+    }
+}
